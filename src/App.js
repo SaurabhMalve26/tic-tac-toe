d52e0cad00ebe95dc28b66a77d49f0fa7c670317
Saurabh Malve <malve.saurabh45@gmail.com>
2024-06-11 12:53:31 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import TicTacToe from './Componemts/TicTacToe/TicTacToe'
+function App() {
+  return (
+    <div>
+     <TicTacToe/>
+    </div>
+  );
+}
+
+export default App;
